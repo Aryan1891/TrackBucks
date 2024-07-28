49af571ce843e38b4076b735924f8da3518c9b99
Aryan1891 <aryanjangiroffical@gmail.com>
2024-07-29 02:45:20 +0530

    Add new pages for MyBanks, Transfer, and Transaction History to the root directory. These pages provide the basic UI structure for the respective functionalities.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
@@ -24,6 +25,11 @@ const Home = () => {
             </header>
             
         </div>
+        <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:123.50},{currentBalance:123.50}]}
+        />
     </section>
   )
 }
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const transactioinHistory = () => {
+  return (
+    <div>transactioinHistory</div>
+  )
+}
+
+export default transactioinHistory
